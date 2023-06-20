6c9b8bcada7cd11abd5acc6064a775bd1fdbac88
Monique <skaiwilliams85@gmail.com>
2023-06-19 20:25:58 -0500

    adjusted modal styles

--- src/components/helpers/ui/Modal.tsx
+++ src/components/helpers/ui/Modal.tsx
@@ -56,13 +56,13 @@ const StyledPrimaryModal = styled(ModalBase)`
 const StyledSecondaryModal = styled(ModalBase)`
   .children {
     background-color: var(--neutral-primary);
-    border-radius: 8px;
+    border-radius: 10px;
     width: calc(100vw - 1rem);
     border: 15px solid var(--neutral-light);
     box-shadow: rgba(17, 17, 26, 0.05) 0px 1px 0px,
       rgba(17, 17, 26, 0.1) 0px 0px 8px;
     padding: 0.5rem;
-    max-width: 450px;
+    max-width: 420px;
 
     .icon-container {
       display: none;
@@ -73,7 +73,6 @@ const StyledSecondaryModal = styled(ModalBase)`
 type Props = {
   children: ReactNode;
   variant?: boolean;
-
   closeModal: () => void;
 };
 

bcf059f40a5f9ff595f8f2c8a90ccbcb1262396f
Monique <skaiwilliams85@gmail.com>
2023-07-23 20:01:24 -0500

    cleaned up code

--- src/main.tsx
+++ src/main.tsx
@@ -32,7 +32,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
-
       <App />
     </Provider>
   </React.StrictMode>
